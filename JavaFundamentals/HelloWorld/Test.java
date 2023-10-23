a257a11908d3bf64caf885cb9d2d49969e0482c8
Juan Sandoval <jcsandoval978@gmail.com>
2023-10-23 17:42:17 -0400

    First Commit

---
+++ JavaFundamentals/HelloWorld/Test.java
@@ -0,0 +1,16 @@
+//* In Java, all functions are written inside of classes, and are called methods.
+
+
+// File name must be the same as the class name
+// !  Every file is a class
+public class Test {
+
+    // The file or class with the exact method signature below 
+    // is alwaus the launching point of the application
+    // You cannot run a Java Program without it.
+
+	public static void main(String[] args) {
+        // For now, all code goes in here
+        System.out.println("Hello World!");
+	}
+}
