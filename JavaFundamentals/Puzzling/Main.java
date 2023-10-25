43fe69e6136b34cc4529acf3f06a2c7fe885f520
Juan Sandoval <jcsandoval978@gmail.com>
2023-10-25 01:58:58 -0400

    Added psuedocode to puzzle

--- JavaFundamentals/Puzzling/Main.java
+++ JavaFundamentals/Puzzling/Main.java
@@ -8,12 +8,19 @@
 
 public class Main {
     public static void main(String[] args) {
+        //! Create an instance of the PuzzleJava class
         PuzzleJava puzzleJava = new PuzzleJava();
+
+        //! Print the result of rolling a 10-sided die 10 times
         System.out.println(puzzleJava.getTenRolls());
 
+        //! Print a random letter
         System.out.println(puzzleJava.getRandomLetter());
+
+        //! Print a random password
         System.out.println(puzzleJava.generatePassword());
 
+        //! Print an array of random passwords of the specified length (param)
         System.out.println(puzzleJava.generateNewPassword(5));
     }
 }
--- JavaFundamentals/Puzzling/PuzzleJava.java
+++ JavaFundamentals/Puzzling/PuzzleJava.java
@@ -4,36 +4,44 @@ import java.util.Random;
 
 public class PuzzleJava {
 
-    public ArrayList<Integer> getTenRolls() {
-        ArrayList<Integer> getTenRolls = new ArrayList<>();
+    public ArrayList<Integer> getTenRolls() { //! Defines a method named getTenRolls and returns an ArrayList of integers (ArrayList<Integer>)
+        ArrayList<Integer> getTenRolls = new ArrayList<>(); //!  Creates the ArrayList of integers named getTenRolls
         Random randMachine = new Random();
         for (int i = 0; i < 10; i++) {
-            getTenRolls.add(randMachine.nextInt(20));
+            getTenRolls.add(randMachine.nextInt(20)); //! Generates a random number between 0 (inclusive) and 20 (exclusive) and adds it to the getTenRolls ArrayList
         }
-        return getTenRolls;
+        return getTenRolls; //! Returns the getTenRolls ArrayList
     };
 
-    public char getRandomLetter() {
-        char[] myArray = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    public char getRandomLetter() { //! Defines a method named getRandomLetter and is designed to return a random letter.
+        char[] myArray = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', //! Creates an array named myArray
                 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
                 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
         Random randMachine = new Random();
-        return myArray[randMachine.nextInt(26)];
+        return myArray[randMachine.nextInt(26)]; 
+            //!  Generates a random integer between 0 (inclusive) and 26 (exclusive) using randMachine.nextInt(26). 
+                //! Uses this random number as an index to access a character in the myArray array.
+                    //! Selected character (letter) is returned as the result of the method.
     };
 
-    public String generatePassword() {
-        String password = "";
-        for (int i = 0; i < 8; i++) {
-            password += getRandomLetter();
+    public String generatePassword() { //! Defines a method named generatePassword and it will return a string
+        String password = ""; //! Created an empty string to push random letters into
+        for (int i = 0; i < 8; i++) { //! Set the length of the password string of random letters 
+            password += getRandomLetter(); 
+                //! Borrow getrandomLetter from same class function to generate random letter and return random char
+                    // ! Add those new char into the password string 8 times
         }
-        return password;
+        return password; //! Returns the created password 
     };
 
-    public ArrayList<String> generateNewPassword(int numofPasswords) {
-        ArrayList<String> passwords = new ArrayList<>();
-        for (int i = 0; i < numofPasswords; i++) {
-            passwords.add(generatePassword());
+    public ArrayList<String> generateNewPassword(int numofPasswords) {  
+        //! Defines a method named generateNewPassword and returns an ArrayList of strings (ArrayList<String>)
+            // ! And takes an integer numofPasswords as a parameter, indicating how many passwords to generate
+        ArrayList<String> passwords = new ArrayList<>(); //! Creates an empty ArrayList named passwords that will store the generated passwords.
+        for (int i = 0; i < numofPasswords; i++) { //! Loop will run as many times as specified by the numofPasswords parameter.
+            passwords.add(generatePassword()); 
+                //! generatePassword method is called to generate a random password. This password is then added to the passwords ArrayList using the add method.
         }
-        return passwords;
+        return passwords; //! Returns the passwords ArrayList
     }
 }
