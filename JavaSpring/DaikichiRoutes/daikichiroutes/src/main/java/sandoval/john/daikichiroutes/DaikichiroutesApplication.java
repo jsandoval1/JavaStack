c0da4158d5e459aa772d61b8b904dc32fa5e76cd
unknown <jcsandoval978@gmail.com>
2023-10-28 10:58:37 -0400

    Completeted Daikichi Routes, practiced Class V. Method annotations

--- JavaSpring/DaikichiRoutes/daikichiroutes/src/main/java/sandoval/john/daikichiroutes/DaikichiroutesApplication.java
+++ JavaSpring/DaikichiRoutes/daikichiroutes/src/main/java/sandoval/john/daikichiroutes/DaikichiroutesApplication.java
@@ -8,21 +8,27 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/Daikichi")
+// Class level annotation
 public class DaikichiroutesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DaikichiroutesApplication.class, args);
 	}
 
-	 // 1. Annotation
-        @RequestMapping("")
-        // 3. Method that maps to the request route above
-        public String index() { // 3
-			return "Hello World!";
-        }
-		@RequestMapping("/world")
-		public String world() {
-            return "Class level annotations are cool too!";
-        }
+	// Method Level Annotation
+	@RequestMapping("/")
+	public String index() {
+		return "Welcome!";
+	}
+
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+
+	@RequestMapping("tomorrow")
+	public String tomorrow() {
+        return "Tomorrow, an oppurtunity will arise, so be sure to be open to new ideas!";
+    }
 }
