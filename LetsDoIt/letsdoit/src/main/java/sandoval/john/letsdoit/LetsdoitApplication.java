315209d837bde31e2847074de97d9f21b8717c20
John Sandoval <jcsandoval978@gmail.com>
2023-11-10 13:42:12 -0500

    Started LetsDoIt folders and POM

---
+++ LetsDoIt/letsdoit/src/main/java/sandoval/john/letsdoit/LetsdoitApplication.java
@@ -0,0 +1,13 @@
+package sandoval.john.letsdoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsdoitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsdoitApplication.class, args);
+	}
+
+}
